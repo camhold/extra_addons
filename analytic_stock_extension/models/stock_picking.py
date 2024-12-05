be99a7532fadf1b8079c9f90eedf30ef4e4f5d9e
Camilo <c.cneiras@gmail.com>
2024-12-04 23:41:22 -0300

    fix_analytic_account_inventary

--- analytic_stock_extension/models/stock_picking.py
+++ analytic_stock_extension/models/stock_picking.py
@@ -1,6 +1,7 @@
 # models/stock_picking.py
 
 from odoo import models, fields, api
+from odoo.exceptions import UserError  
 
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
@@ -14,34 +15,43 @@ class StockPicking(models.Model):
         res = super(StockPicking, self).button_validate()
 
         for picking in self:
-            if picking.picking_type_id.code == 'internal' and picking.location_dest_id.usage == 'production':
-                for move in picking.move_ids:
-                    move.analytic_account_ids = picking.analytic_account_ids
-                    for move_line in move.move_line_ids:
-                        move_line.analytic_account_ids = picking.analytic_account_ids
+             if picking.picking_type_id.code == 'internal' and picking.location_dest_id.usage == 'production':
+                # Validar que se ingresen exactamente 2 cuentas analíticas
+                if len(picking.analytic_account_ids) != 2:
+                    raise UserError("Error: Se deben ingresar dos cuentas analíticas (Centro de Costo y Proyecto) para continuar con la validación.")
+                 # Validar que las cuentas analíticas sean de distintos planes analíticos
+                analytic_plans = picking.analytic_account_ids.mapped('plan_id')
+                if len(set(analytic_plans)) != 2:
+                    raise UserError("Error: Las dos cuentas analíticas deben ser de distintos planes analíticos.")
 
-                    account_move = move.account_move_ids[:1]
-                    if account_move:
-                        account_move.sudo()
-                        for line in account_move.line_ids:
-                            ids = [str(analytic_account.id) for analytic_account in picking.analytic_account_ids[:2]]
-                            analytic_distribution = {",".join(ids): 100}
+                if picking.picking_type_id.code == 'internal' and picking.location_dest_id.usage == 'production':
+                    for move in picking.move_ids:
+                        move.analytic_account_ids = picking.analytic_account_ids
+                        for move_line in move.move_line_ids:
+                            move_line.analytic_account_ids = picking.analytic_account_ids
 
-                            if picking.location_id.usage == 'production' and line.debit != 0.0:
-                                line.sudo().write({
-                                    'analytic_distribution': analytic_distribution
-                                })
-                            elif picking.location_dest_id.usage == 'production' and line.credit != 0.0:
-                                line.sudo().write({
-                                    'analytic_distribution': analytic_distribution
-                                })
+                        account_move = move.account_move_ids[:1]
+                        if account_move:
+                            account_move.sudo()
+                            for line in account_move.line_ids:
+                                ids = [str(analytic_account.id) for analytic_account in picking.analytic_account_ids[:2]]
+                                analytic_distribution = {",".join(ids): 100}
 
-                            # Distribuir en la contracuenta
-                            contra_line = account_move.line_ids.filtered(lambda l: l.account_id.id != line.account_id.id and l.id != line.id)
-                            if contra_line:
-                                contra_line.sudo().write({
-                                    'analytic_distribution': analytic_distribution
-                                })
+                                if picking.location_id.usage == 'production' and line.debit != 0.0:
+                                    line.sudo().write({
+                                        'analytic_distribution': analytic_distribution
+                                    })
+                                elif picking.location_dest_id.usage == 'production' and line.credit != 0.0:
+                                    line.sudo().write({
+                                        'analytic_distribution': analytic_distribution
+                                    })
+
+                                # Distribuir en la contracuenta
+                                contra_line = account_move.line_ids.filtered(lambda l: l.account_id.id != line.account_id.id and l.id != line.id)
+                                if contra_line:
+                                    contra_line.sudo().write({
+                                        'analytic_distribution': analytic_distribution
+                                    })
 
         return res
 
